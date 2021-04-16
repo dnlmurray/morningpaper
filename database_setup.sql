4ae47ac62b46debc397b94748b96fe9ab054c387
Pavel Egorov <pavel.egorov.4227@yandex.ru>
2021-04-17 00:08:28 +0300

    Removed redundant "public" schema name from all entities

--- database_setup.sql
+++ database_setup.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE morningpaper;
 
-CREATE TABLE public.users (
+CREATE TABLE users (
 	id serial NOT NULL,
 	uid bigint NOT NULL,
 	preferred_time time,
@@ -8,10 +8,10 @@ CREATE TABLE public.users (
 	CONSTRAINT users_pk PRIMARY KEY (id)
 );
 
-ALTER TABLE public.users OWNER TO postgres;
+ALTER TABLE users OWNER TO postgres;
 
 
-CREATE TABLE public.news (
+CREATE TABLE news (
 	id serial NOT NULL,
 	heading varchar(280) NOT NULL,
 	summary varchar(1000),
@@ -23,49 +23,49 @@ CREATE TABLE public.news (
 	CONSTRAINT news_pk PRIMARY KEY (id)
 
 );
-ALTER TABLE public.news OWNER TO postgres;
+ALTER TABLE news OWNER TO postgres;
 
-CREATE TABLE public.topics (
+CREATE TABLE topics (
 	id serial NOT NULL,
 	name varchar(30) NOT NULL,
 	CONSTRAINT topics_pk PRIMARY KEY (id)
 
 );
-ALTER TABLE public.topics OWNER TO postgres;
+ALTER TABLE topics OWNER TO postgres;
 
-CREATE TABLE public.sources (
+CREATE TABLE sources (
 	id serial NOT NULL,
 	name varchar(30) NOT NULL,
 	link varchar(80),
 	CONSTRAINT sources_pk PRIMARY KEY (id)
 
 );
-ALTER TABLE public.sources OWNER TO postgres;
+ALTER TABLE sources OWNER TO postgres;
 
-ALTER TABLE public.news ADD CONSTRAINT fk_sources FOREIGN KEY (sources_id)
-REFERENCES public.sources (id) MATCH FULL
+ALTER TABLE news ADD CONSTRAINT fk_sources FOREIGN KEY (sources_id)
+REFERENCES sources (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-CREATE TABLE public.many_users_has_many_topics (
+CREATE TABLE many_users_has_many_topics (
 	users_id integer NOT NULL,
 	id_topics integer NOT NULL,
 	CONSTRAINT many_users_has_many_topics_pk PRIMARY KEY (users_id,id_topics)
 
 );
 
-ALTER TABLE public.many_users_has_many_topics ADD CONSTRAINT fk_users FOREIGN KEY (users_id)
-REFERENCES public.users (id) MATCH FULL
+ALTER TABLE many_users_has_many_topics ADD CONSTRAINT fk_users FOREIGN KEY (users_id)
+REFERENCES users (id) MATCH FULL
 ON DELETE RESTRICT ON UPDATE CASCADE;
 
-ALTER TABLE public.many_users_has_many_topics ADD CONSTRAINT fk_topics FOREIGN KEY (id_topics)
-REFERENCES public.topics (id) MATCH FULL
+ALTER TABLE many_users_has_many_topics ADD CONSTRAINT fk_topics FOREIGN KEY (id_topics)
+REFERENCES topics (id) MATCH FULL
 ON DELETE RESTRICT ON UPDATE CASCADE;
 
-ALTER TABLE public.news ADD CONSTRAINT fk_topics FOREIGN KEY (topics_id)
-REFERENCES public.topics (id) MATCH FULL
+ALTER TABLE news ADD CONSTRAINT fk_topics FOREIGN KEY (topics_id)
+REFERENCES topics (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-CREATE TABLE public.weather (
+CREATE TABLE weather (
 	id serial NOT NULL,
 	temperature real,
 	cloudiness smallint,
@@ -75,9 +75,9 @@ CREATE TABLE public.weather (
 	CONSTRAINT weather_pk PRIMARY KEY (id)
 
 );
-ALTER TABLE public.weather OWNER TO postgres;
+ALTER TABLE weather OWNER TO postgres;
 
-CREATE TABLE public.locations (
+CREATE TABLE locations (
 	id serial NOT NULL,
 	location varchar(30),
 	lat real NOT NULL,
@@ -85,26 +85,26 @@ CREATE TABLE public.locations (
 	CONSTRAINT locations_pk PRIMARY KEY (id)
 
 );
-ALTER TABLE public.locations OWNER TO postgres;
+ALTER TABLE locations OWNER TO postgres;
 
-ALTER TABLE public.weather ADD CONSTRAINT fk_locations FOREIGN KEY (locations_id)
-REFERENCES public.locations (id) MATCH FULL
+ALTER TABLE weather ADD CONSTRAINT fk_locations FOREIGN KEY (locations_id)
+REFERENCES locations (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE public.users ADD CONSTRAINT fk_locations FOREIGN KEY (locations_id)
-REFERENCES public.locations (id) MATCH FULL
+ALTER TABLE users ADD CONSTRAINT fk_locations FOREIGN KEY (locations_id)
+REFERENCES locations (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-CREATE TABLE public.currencies (
+CREATE TABLE currencies (
 	id serial NOT NULL,
 	name varchar(30),
 	abbreviation varchar(3),
 	CONSTRAINT currencies_pk PRIMARY KEY (id)
 
 );
-ALTER TABLE public.currencies OWNER TO postgres;
+ALTER TABLE currencies OWNER TO postgres;
 
-CREATE TABLE public.exchange_rates (
+CREATE TABLE exchange_rates (
 	id serial NOT NULL,
 	base integer,
 	target integer,
@@ -112,40 +112,40 @@ CREATE TABLE public.exchange_rates (
 	CONSTRAINT exchange_rates_pk PRIMARY KEY (id)
 
 );
-ALTER TABLE public.exchange_rates OWNER TO postgres;
+ALTER TABLE exchange_rates OWNER TO postgres;
 
-ALTER TABLE public.exchange_rates ADD CONSTRAINT currencies_fk FOREIGN KEY (base)
-REFERENCES public.currencies (id) MATCH FULL
+ALTER TABLE exchange_rates ADD CONSTRAINT currencies_fk FOREIGN KEY (base)
+REFERENCES currencies (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE public.exchange_rates ADD CONSTRAINT currencies_fk1 FOREIGN KEY (target)
-REFERENCES public.currencies (id) MATCH FULL
+ALTER TABLE exchange_rates ADD CONSTRAINT currencies_fk1 FOREIGN KEY (target)
+REFERENCES currencies (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-CREATE TABLE public.users_currencies (
+CREATE TABLE users_currencies (
 	users_id integer,
 	base integer,
 	target_one integer,
 	target_two integer
 );
-ALTER TABLE public.users_currencies OWNER TO postgres;
+ALTER TABLE users_currencies OWNER TO postgres;
 
-ALTER TABLE public.users_currencies ADD CONSTRAINT users_fk FOREIGN KEY (users_id)
-REFERENCES public.users (id) MATCH FULL
+ALTER TABLE users_currencies ADD CONSTRAINT users_fk FOREIGN KEY (users_id)
+REFERENCES users (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE public.users_currencies ADD CONSTRAINT users_currencies_uq UNIQUE (users_id);
+ALTER TABLE users_currencies ADD CONSTRAINT users_currencies_uq UNIQUE (users_id);
 
-ALTER TABLE public.users_currencies ADD CONSTRAINT currencies_fk FOREIGN KEY (target_one)
-REFERENCES public.currencies (id) MATCH FULL
+ALTER TABLE users_currencies ADD CONSTRAINT currencies_fk FOREIGN KEY (target_one)
+REFERENCES currencies (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE public.users_currencies ADD CONSTRAINT currencies_fk1 FOREIGN KEY (target_two)
-REFERENCES public.currencies (id) MATCH FULL
+ALTER TABLE users_currencies ADD CONSTRAINT currencies_fk1 FOREIGN KEY (target_two)
+REFERENCES currencies (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE public.users_currencies ADD CONSTRAINT currencies_fk2 FOREIGN KEY (base)
-REFERENCES public.currencies (id) MATCH FULL
+ALTER TABLE users_currencies ADD CONSTRAINT currencies_fk2 FOREIGN KEY (base)
+REFERENCES currencies (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 
 
