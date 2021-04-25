b02ac23044778aa0648f0e1db151d743a0e3128b
Pavel Egorov <pavel.egorov.4227@yandex.ru>
2021-04-26 00:04:23 +0300

    Basic keyboard with topics + improved /cancel command

--- src/negotiator.py
+++ src/negotiator.py
@@ -3,6 +3,10 @@ import sys
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from aiogram.utils import executor
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
@@ -15,6 +19,11 @@ if not BOT_TOKEN:
     sys.exit("No bot token was found in ENV. Set 'BOT_TOKEN' variable to your token from @BotFather")
 bot = Bot(BOT_TOKEN)
 dispatcher = Dispatcher(bot, storage=MemoryStorage())  # TODO: Consider using more advanced storage
+
+
+class TopicSetter(StatesGroup):
+    process_topics = State()
+    apply_results = State()
 
 
 def init():
@@ -37,13 +46,47 @@ async def send_hello(message: types.message):
 @dispatcher.message_handler(commands='help')
 async def send_help(message: types.message):
     await message.answer("I can send you fresh news everyday.\n"
-                         "My commands are: /topics, /currency, /city, /time, /review and /help")
+                         "My commands are: /topics, /currency, /city, /time, /review, /cancel /help")
+
+
+@dispatcher.message_handler(commands='cancel', state='*')
+@dispatcher.message_handler(Text(equals='cancel', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        await message.answer('There is nothing to cancel')
+        return
+    await state.finish()
+    await message.answer('Action cancelled', reply_markup=ReplyKeyboardRemove())
+
+
+topics_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+topics_keyboard.row(
+    KeyboardButton('Business'),
+    KeyboardButton('Entertainment'),
+    KeyboardButton('Technology')
+)
+topics_keyboard.row(
+    KeyboardButton('Health'),
+    KeyboardButton('Science'),
+    KeyboardButton('Sports'),
+    KeyboardButton('General')
+)
+topics_keyboard.row(
+    KeyboardButton('Cancel'),
+    KeyboardButton('Apply')
+)
 
 
 @dispatcher.message_handler(commands='topics')
 async def set_topics(message: types.message):
-    await message.answer("This command will allow you to set topics of the news you want to get.\n"
-                         "Currently does nothing")
+    await TopicSetter.process_topics.set()
+    await message.answer("Now you can edit news topics", reply_markup=topics_keyboard)
+
+
+@dispatcher.message_handler(state=TopicSetter.process_topics)
+async def process_topics(message: types.Message, state: FSMContext):
+    await message.answer('Your current topics are:', reply_markup=topics_keyboard)
 
 
 @dispatcher.message_handler(commands='currency')
